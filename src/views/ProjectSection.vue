f758580e12e984ede2ac977f30af4b5ef0c73d42
jorce <17802926729@163.com>
2018-09-14 11:19:11 +0800

    修复了学生管理中某学生课程<总课程的问题

--- src/views/ProjectSection.vue
+++ src/views/ProjectSection.vue
@@ -48,103 +48,105 @@
 </template>
 
 <script>
-
 export default {
-  name: 'home',
-  data (){
-    return{
-       currentUser: '',
-      userProjects: [],
+  name: "home",
+  data() {
+    return {
+      currentUser: "",
+      userProjects: []
+    };
+  },
+  methods: {
+    handleClick: function(row) {
+      // this.$router.push({name: '/projectdetail.html#'+row.projectId,});
+      window.location.href = `/#${row.projectId}`;
     }
   },
-  methods:{
-     handleClick:function(row){
-      // this.$router.push({name: '/projectdetail.html#'+row.projectId,});
-       window.location.href = `/#${row.projectId}`
-     }
-  },
-   mounted() {
-      let user = sessionStorage.getItem('user')
-      this.currentUser = JSON.parse(user);
-          let url = this.url +'/login/getProjectBySection'
-          this.axios.get(url, {params:{name: this.currentUser.userid}})
-          .then((res) => {
-            var num = []
-            var noNum = []
-            // 判断是否含有数字章节开头
-            res.data.forEach(item => {
-              if(item.section.substr(0,1) == '1'||item.section.substr(0,1) == '2'||item.section.substr(0,1) == '3'){
-                num.push(item)
-              }else {
-                noNum.push(item)
-              }
-            });
-            console.log(num.concat(noNum));
-            this.userProjects = num.concat(noNum)
-            .sort((obj1, obj2) => {
-                let num1 = Number(obj1.section.substr(0, 3).replace('.', ''))
-                let num2 = Number(obj2.section.substr(0, 3).replace('.', ''))
-                return num1 - num2
-              })
-            
-          })
-          .catch((err) => {
-              console.log(err);
-          })
-      }
-}
+  mounted() {
+    let user = sessionStorage.getItem("user");
+    this.currentUser = JSON.parse(user);
+    let url = this.url + "/login/getProjectBySection";
+    this.axios
+      .get(url, { params: { name: this.currentUser.userid } })
+      .then(res => {
+        var num = [];
+        var noNum = [];
+        // 判断是否含有数字章节开头
+        res.data.forEach(item => {
+          if (
+            item.section.substr(0, 1) == "1" ||
+            item.section.substr(0, 1) == "2" ||
+            item.section.substr(0, 1) == "3"
+          ) {
+            num.push(item);
+          } else {
+            noNum.push(item);
+          }
+        });
+        console.log(num.concat(noNum));
+        this.userProjects = num.concat(noNum).sort((obj1, obj2) => {
+          let num1 = Number(obj1.section.substr(0, 3).replace(".", ""));
+          let num2 = Number(obj2.section.substr(0, 3).replace(".", ""));
+          return num1 - num2;
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
 </script>
  <style scoped>
-    .container {
-      display: flex;
-      width: 80%;
-      justify-content: center;
-      align-items: center;
-      /* width: 1440px; */
-    }
+.container {
+  display: flex;
+  width: 80%;
+  justify-content: center;
+  align-items: center;
+  /* width: 1440px; */
+}
 
-    .main {
-      width: 1040px;
-      margin: 70px auto 140px;
-      overflow: hidden;
-    }
+.main {
+  width: 1040px;
+  margin: 70px auto 140px;
+  overflow: hidden;
+}
 
-    .project-name {
-      float: left;
-      width: 242px;
-      height: 270px;
-      margin: 8px 8px;
-      background-color: #fff;
-      box-sizing: border-box;
-      padding: 30px;
-      cursor: pointer;
-    }
+.project-name {
+  float: left;
+  width: 242px;
+  height: 270px;
+  margin: 8px 8px;
+  background-color: #fff;
+  box-sizing: border-box;
+  padding: 30px;
+  cursor: pointer;
+}
 
-    .project-name:hover {
-      box-shadow: 0 17px 10px -10px rgba(0, 0, 0, 0.13);
-    }
+.project-name:hover {
+  box-shadow: 0 17px 10px -10px rgba(0, 0, 0, 0.13);
+}
 
-    .project-name div:first-child {
-      /* color: #aaa; */
-    }
+.project-name div:first-child {
+  /* color: #aaa; */
+}
 
-    .project-title {
-      /* font-family: SourceHanSansCN-Bold; */
-      font-weight: 500;
-      font-size: 24px;
-      line-height: 40px;
-      color: #4E4E4E;
-      text-align: center;
-      letter-spacing: 0.2px;
-      margin-bottom: 20px;
-    }
-   
-    .project-title.project-title-home>div {
-      line-height: 270px;
-      color: #4e4e4e;
-    }
+.project-title {
+  /* font-family: SourceHanSansCN-Bold; */
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 40px;
+  color: #4e4e4e;
+  text-align: center;
+  letter-spacing: 0.2px;
+  margin-bottom: 20px;
+}
 
-    /* .project-time {
+.project-title.project-title-home > div {
+  line-height: 270px;
+  color: #4e4e4e;
+}
+
+/* .project-time {
       font-family: SourceHanSansCN-Normal;
       font-size: 12px;
       line-height: 24px;
@@ -152,20 +154,19 @@ export default {
       letter-spacing: 0.1px;
     } */
 
-    a {
-      text-decoration: none;
-      color: #000;
-    }
-    .el-table th{
-      background-color: #F5F5F5;
-      color: #B4BFC6 ;
-      font-size: 14px;
-    }
-    .el-table{
-      font-size: 14px;
-    }
-    .el-table_1_column_1 {
-      padding-left: 20px !important;
-    }
-
-  </style>
+a {
+  text-decoration: none;
+  color: #000;
+}
+.el-table th {
+  background-color: #f5f5f5;
+  color: #b4bfc6;
+  font-size: 14px;
+}
+.el-table {
+  font-size: 14px;
+}
+.el-table_1_column_1 {
+  padding-left: 20px !important;
+}
+</style>
--- src/views/Student.vue
+++ src/views/Student.vue
@@ -27,6 +27,7 @@
           <p v-if="scope.row.projectList[inner].status===0" class="notScore">{{scope.row.projectList[inner].score}}</p>
           <p v-if="scope.row.projectList[inner].status===1" class="score" @click="toDetail(scope.row.projectList[inner].projectId)">{{scope.row.projectList[inner].score}}</p>
           <p v-if="scope.row.projectList[inner].status===2" class="scored" @click="toDetail(scope.row.projectList[inner].projectId)">{{scope.row.projectList[inner].score}}</p>
+          <p v-if="scope.row.projectList[inner].status === 4">{{scope.row.projectList[inner].score}}</p>
         </template>
       </el-table-column>
     <!-- </div> -->
@@ -53,35 +54,8 @@ export default {
   data (){
     return{
       currentUserId :'',
-      tableData: [{
-          date: '2016-05-03',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-02',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-      }],
+      tableData: [],
+      mockData: [{"studentID":"embark003","studentName":"王欣","projectList":[{"projectId":4538783999459328,"projectName":"IAD_1_150_Camera","projectSection":"","pos":4,"score":-1,"status":0},{"projectId":4996180836614144,"projectName":"IAD_1_110","projectSection":"","pos":0,"score":-1,"status":0},{"projectId":5101733952880640,"projectName":"IAD_1_210_documents","projectSection":"","pos":6,"score":-1,"status":0},{"projectId":5559130790035456,"projectName":"IAD_1_130_FirstApp","projectSection":"","pos":2,"score":-1,"status":0},{"projectId":5664683906301952,"projectName":"IAD_1_Milestone1","projectSection":"","pos":5,"score":-1,"status":0},{"projectId":6122080743456768,"projectName":"IAD_1_120","projectSection":"","pos":1,"score":-1,"status":0}]},{"studentID":"embark002","studentName":"马晓东","projectList":[{"projectId":4573968371548160,"projectName":"IAD_1_210_documents","projectSection":"","pos":6,"score":-1,"status":0},{"projectId":4925812092436480,"projectName":"IAD_1_130_FirstApp","projectSection":"","pos":2,"score":-1,"status":0},{"projectId":5207287069147136,"projectName":"IAD_1_110","projectSection":"","pos":0,"score":-1,"status":0},{"projectId":5488762045857792,"projectName":"IAD_1_150_Camera","projectSection":"","pos":4,"score":-1,"status":0},{"projectId":5699868278390784,"projectName":"IAD_1_210_notepad","projectSection":"","pos":7,"score":-1,"status":0},{"projectId":6051711999279104,"projectName":"IAD_1_140_Drawing","projectSection":"","pos":3,"score":-1,"status":0},{"projectId":6333186975989760,"projectName":"IAD_1_120","projectSection":"","pos":1,"score":-1,"status":0},{"projectId":6614661952700416,"projectName":"IAD_1_Milestone1","projectSection":"","pos":5,"score":-1,"status":0}]},{"studentID":"embark001","studentName":"方小达","projectList":[{"projectId":4644337115725824,"projectName":"IAD_1_210_notepad","projectSection":"","pos":7,"score":-1,"status":0},{"projectId":4785074604081152,"projectName":"IAD_1_140_Drawing","projectSection":"","pos":3,"score":-1,"status":0},{"projectId":5066549580791808,"projectName":"IAD_1_120","projectSection":"","pos":1,"score":-1,"status":0},{"projectId":5348024557502464,"projectName":"IAD_1_Milestone1","projectSection":"","pos":5,"score":-1,"status":0},{"projectId":5629499534213120,"projectName":"IAD_1_110","projectSection":"","pos":0,"score":-1,"status":0},{"projectId":5910974510923776,"projectName":"IAD_1_150_Camera","projectSection":"","pos":4,"score":-1,"status":0},{"projectId":6192449487634432,"projectName":"IAD_1_130_FirstApp","projectSection":"","pos":2,"score":-1,"status":0},{"projectId":6473924464345088,"projectName":"IAD_1_210_documents","projectSection":"","pos":6,"score":-1,"status":0}]}],
       studentHeader: [
         {title:'1.1.App开发课程简介'},
         {title:'1.2.项目新建'},
@@ -121,26 +95,43 @@ export default {
             .then((res) => {
               // this.student = res.data
               let temp = res.data;
+              // let temp = this.mockData;
+              let len = 0
               temp.forEach(element => {
+                if (element.projectList.length>len) {
+                  len = element.projectList.length
+                }
                 element.projectList.sort((a,b)=>a.pos-b.pos);
               });
               // this.tableData = temp;
+              
               temp.forEach(element => {
+                if (element.projectList.length<len) {
+                  for (let index = element.projectList.length; index < len; index++) {
+                      element.projectList[index]= {}
+                  }
+                }
                 for (let index = 0; index < element.projectList.length; index++) {
-                  element.projectList[index].title =  this.studentHeader[index].title
                   if (element.projectList[index].status===0) {
                     element.projectList[index].score = '未提交'
                   }else if (element.projectList[index].status===1) {
                     element.projectList[index].score = '未打分'
                   }
-                  
+                  if (element.projectList[index].status === undefined ) {
+                    element.projectList[index] = {
+                      score:'未选课',
+                      status:4
+                    }
+                  }
+                  element.projectList[index].title =  this.studentHeader[index].title
                   // else if (element.projectList[index].status===2) {
                   //   element.projectList[index].score = '已打分'
                   // }
                   // element.projectList[index].score = element.projectList[index].score.toString()
                 }
               });
-              this.studentHeader = this.studentHeader.splice(0,temp[0].projectList.length);
+              
+              this.studentHeader = this.studentHeader.splice(0,len);
               // console.log(this.studentHeader);
               this.tableData = temp;
 
